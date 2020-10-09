d025e9b7fd7ff7695d6c9d4af6486e44e7f79cbe
edwardzhang5 <edwardzhang5@gmail.com>
2020-10-09 11:49:53 -0400

    new with comments

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 Author: Edward Zhang
 Title: AVL Project
 Date: 10/9/2020
-Purpose: This project creates an AVL data structure to balance GatorIds
+Purpose: This project creates an AVL data structure to store GatorIds
 */
 
 #include <iostream>
@@ -36,59 +36,44 @@ public:
 class AVL {
 public:
 	// Default Constructor
-	AVL() 
+	AVL()
 	{
 		this->root = nullptr;
 		nodeCount = 0;
 	}
-	void printInorder() 
+
+	// Adds a student object into tree with given name, ID
+	void insert(string name, int val) {
+		root = insertNode(root, name, val);
+		root = balance(root);
+		nodeCount++;
+	}
+
+	// Recursive function for inserting node
+	Node* insertNode(Node* root, string name, int val)
 	{
-		if (nodeCount != 0) {
-			string s = printInorderR(root);
-			cout << s.substr(2) << endl;
+		if (root == nullptr) {
+			root = new Node(name, val);
+			root->height = 1;
+			cout << "successful" << endl;
+			return root;
 		}
-		else
+		if (val > root->val)
+			root->right = insertNode(root->right, name, val);
+		else if (val < root->val)
+			root->left = insertNode(root->left, name, val);
+		else {
 			cout << "unsuccessful" << endl;
-	}
-	string printInorderR(Node* node) 
-	{
-		// Stepik Code
-		// https://stepik.org/submissions/1480359?unit=379726
-		string temp;
-		if (node == nullptr)
-			return "";
-		temp = printInorderR(node->left) + ", " + node->name + printInorderR(node->right);
-		return temp;
-	}
-	void printPreorder() 
-	{
-		string s = printPreorderR(root);
-		cout << s.substr(2) << endl;
-	}
-	string printPreorderR(Node* node) 
-	{
-		string temp;
-		if (node == nullptr)
-			return "";
-		temp = ", " + node->name+ printPreorderR(node->left) +  printPreorderR(node->right);
-		return temp;
-	}
-	void printPostorder() 
-	{
-		string s = printPostorderR(root);
-		cout << s.substr(2) << endl;
-	}
-	string printPostorderR(Node* node) 
-	{
-		string temp;
-		if (node == nullptr)
-			return "";
-		temp =  printPostorderR(node->left) + printPostorderR(node->right) + ", " + node->name;
-		return temp;
+			return nullptr;
+		}
+		root->left = balance(root->left);
+		root->right = balance(root->right);
+		root->height = height(root);
+		return root;
 	}
 
-
-	Node* balance(Node* node) 
+	// Code to balance each root involved in insertion
+	Node* balance(Node* node)
 	{
 		if (node == nullptr) {
 			return node;
@@ -120,48 +105,24 @@ public:
 		return node;
 
 	}
-	int balanceFactor(Node* root) 
+
+	// Helper function to calculate balance factor, uses height function
+	int balanceFactor(Node* root)
 	{
 		if (root == nullptr)
 			return 0;
 		return height(root->left) - height(root->right);
 	}
 
-	void insert(string name, int val) {
-		// Adds a student object into tree with given name, ID
-		root = insertNode(root, name, val);
-		root = balance(root);
-		nodeCount++;
-	}
-	Node* insertNode(Node* root, string name, int val) 
-	{
-		if (root == nullptr) {
-			root = new Node(name, val);
-			root->height = 1;
-			cout << "successful" << endl;
-			return root;
-		}
-		if (val > root->val)
-			root->right = insertNode(root->right, name, val);
-		else if (val < root->val)
-			root->left = insertNode(root->left, name, val);
-		else {
-			cout << "unsuccessful" << endl;
-			return nullptr;
-		}
-		root->left = balance(root->left);
-		root->right = balance(root->right);
-		root->height = height(root);
-		return root;
-	}
-
-	void remove(int val) 
+	// Called in main to remove specified ID, uses recursive removeR function
+	void remove(int val)
 	{
 		bool s = false;
 		if (nodeCount == 0) {
 			cout << "unsuccessful" << endl;
 			return;
 		}
+		// Recursive function sets s to true if successful
 		root = removeR(val, root, s);
 		if (!s)
 			cout << "unsuccessful" << endl;
@@ -171,13 +132,13 @@ public:
 		}
 
 	}
-	Node* removeR(int val, Node* node, bool &s)
+	// Recursive function to find and remove ID, sets s to true if successful
+	Node* removeR(int val, Node* node, bool& s)
 	{
 		//Find and remove account with specified ID
 		if (node == nullptr)
 			return node;
 		else if (node->val < val) {
-			
 			node->right = removeR(val, node->right, s);
 		}
 		else if (node->val > val) {
@@ -185,14 +146,14 @@ public:
 		}
 		else {
 			s = true;
-			
+			// Calls helper function to delete node
 			return deleteNode(node);
 		}
 		return node;
 
 	}
-	
-	Node* deleteNode(Node* node) 
+	// Helper function to delete a specific node
+	Node* deleteNode(Node* node)
 	{
 		if (node->left == nullptr && node->right == nullptr) {
 			// Leaf node
@@ -211,8 +172,8 @@ public:
 			delete node;
 			return temp;
 		}
-		else { 			// Two children
-
+		else {
+			// Two children
 			// Finds in order successor, leftmost node on right side
 			Node* inOrderS = node->right;
 			while (inOrderS->left != nullptr) {
@@ -228,69 +189,46 @@ public:
 		return node;
 	}
 
-
-	void removeInorder(int n) 
-	{
-		// check of input
-		if (n >= nodeCount||n < 0) {
-			cout << "unsuccessful" << endl;
-			return;
-		}
-		vector<Node*> nodeList;
-		nodeCount--;
-		int counter = 0;
-		root = removeInorderR(root, n+1, counter);
-		cout << "successful" << endl;
-	}
-
-	Node* removeInorderR(Node* node, int n, int &currCount) 
-	{
-		if (node == nullptr)
-			return node;
-		node->left = removeInorderR(node->left, n, currCount);
-		currCount++;
-		if (currCount == n) {
-			return deleteNode(node);
-		}
-		else if (currCount > n) {
-			return node;
-		}
-		node->right = removeInorderR(node->right, n, currCount);
-		
-		return node;
-
-	}
-
-	void search(int val) 
+	// Calls searchNode method if input is an integer
+	void search(int val)
 	{
 		if (nodeCount == 0) {
 			cout << "unsuccessful" << endl;
 			return;
 		}
-		if (!searchNode(val, root))
+		bool found = false;
+		searchNode(val, root, found);
+		if (!found)
 			cout << "unsuccessful" << endl;
 	}
 
-	bool searchNode(int val, Node* node)
+	// Returns false if can't find given node, prints the node if the id exists
+	void searchNode(int val, Node* node, bool& found)
 	{
 		// Search for student with specifed ID, prints name
 		// Returns false if ID isn't found
-		if (node == nullptr)
-			return false;
+		if (node == nullptr || found)
+			return;
 		if (node->val == val) {
 			cout << node->name << endl;
-			return true;
+			found = !found;
+			return;
 		}
-		return searchNode(val, node->left) || searchNode(val, node->right);
+		if (val > node->val) 
+			searchNode(val, node->right, found);
+		else if(val<node->val)
+			searchNode(val, node->left, found);
+		return;
 	}
 
-
-	void search(string name) 
+	// Overloaded search function to find IDs if input is a string
+	void search(string name)
 	{
 		if (nodeCount == 0) {
 			cout << "unsuccessful" << endl;
 			return;
 		}
+		// Stores matching IDs in a vector
 		vector<string> IDMatch;
 		searchName(name, root, IDMatch);
 		if (IDMatch.empty())
@@ -300,9 +238,9 @@ public:
 				cout << IDMatch[i] << endl;
 			}
 		}
-			
 	}
-	void searchName(string n, Node* node,vector<string> &s) 
+	// Recursive function for finding all matching names
+	void searchName(string n, Node* node, vector<string>& s)
 	{
 		if (node == nullptr) {
 			return;
@@ -310,11 +248,12 @@ public:
 		if ((node->name).compare(n) == 0) {
 			s.push_back(printID(node->val));
 		}
-		searchName(n, node->left,s);
-	    searchName(n, node->right,s);
+		searchName(n, node->left, s);
+		searchName(n, node->right, s);
 	}
 
-	string printID(int id) 
+	// Helper function to convert integer ID to 8 digit string
+	string printID(int id)
 	{
 		string r = to_string(id);
 		while (r.length() < 8) {
@@ -322,31 +261,125 @@ public:
 		}
 		return r;
 	}
-	void printLevelCount() 
+
+	// Function call to printInorder, calls recursive function
+	void printInorder()
+	{
+		if (nodeCount != 0) {
+			string s = printInorderR(root);
+			// Ensures no leading comma in output
+			cout << s.substr(2) << endl;
+		}
+		else
+			cout << "" << endl;
+	}
+	// Recursive call to store inOrder traversal in comma delimited string
+	string printInorderR(Node* node)
+	{
+		// Stepik Code
+		// https://stepik.org/submissions/1480359?unit=379726
+		string temp;
+		if (node == nullptr)
+			return "";
+		temp = printInorderR(node->left) + ", " + node->name + printInorderR(node->right);
+		return temp;
+	}
+
+	// Function to print preOrderTraversal of tree
+	void printPreorder()
+	{
+		// Calls Recursive function, removes leading comma
+		if (nodeCount != 0) {
+			string s = printPreorderR(root);
+			// Ensures no leading comma in output
+			cout << s.substr(2) << endl;
+		}
+		else
+			cout << "" << endl;
+	}
+	// Recursive preOrder traversal
+	string printPreorderR(Node* node)
+	{
+		string temp;
+		if (node == nullptr)
+			return "";
+		temp = ", " + node->name + printPreorderR(node->left) + printPreorderR(node->right);
+		return temp;
+	}
+
+	// Function to print post order traversal of tree
+	void printPostorder()
+	{
+		if (nodeCount != 0) {
+			string s = printPostorderR(root);
+			// Ensures no leading comma in output
+			cout << s.substr(2) << endl;
+		}
+		else
+			cout << "" << endl;
+	}
+	// Recursive function to combine entire tree into comma delimited line
+	string printPostorderR(Node* node)
+	{
+		string temp;
+		if (node == nullptr)
+			return "";
+		temp = printPostorderR(node->left) + printPostorderR(node->right) + ", " + node->name;
+		return temp;
+	}
+
+	// Prints the number of levels in the tree, or height of the root
+	// Outputs 0 if root==nullptr
+	void printLevelCount()
 	{
 		cout << height(root) << endl;
 	}
-	bool checkAVL() 
+
+	// Call to remove specified nth gatorID from inorder traversal
+	void removeInorder(int n)
 	{
-		return checkAVL(root);
+		// check of input
+		if (n >= nodeCount || n < 0) {
+			cout << "unsuccessful" << endl;
+			return;
+		}
+		vector<Node*> nodeList;
+		nodeCount--;
+		// Counter is a way to keep track if the nth id has been reached yet
+		int counter = 0;
+		root = removeInorderR(root, n + 1, counter);
+		cout << "successful" << endl;
 	}
-	bool checkAVL(Node* root) {
-		// From my Stepik, checks if tree is AVL
-		// https://stepik.org/submissions/1590562?unit=400654
-		// Helper function for me to check
-		if (root == nullptr)
-			return true;
-		if (abs(height(root->left) - height(root->right)) > 1)
-			return false;
-		return checkAVL(root->left) && checkAVL(root->right);
+
+	Node* removeInorderR(Node* node, int n, int& currCount)
+	{
+		if (node == nullptr)
+			return node;
+		node->left = removeInorderR(node->left, n, currCount);
+
+		// currCount keeps track if the nth root has been reached yet
+		currCount++;
+		if (currCount == n) {
+			return deleteNode(node);
+		}
+		else if (currCount > n) {
+			// Breaks out of recursive call if nth root has been reached
+			return node;
+		}
+		node->right = removeInorderR(node->right, n, currCount);
+
+		return node;
+
 	}
-	int height(Node* root)
+
+	// Helper function to find height of a node
+	int height(Node* node)
 	{
 		// From my Stepik Code, recursive height function
 		// https://stepik.org/submissions/1590562?unit=400654
-		if (root == nullptr)
+		if (node == nullptr)
 			return 0;
-		return 1 + max(height(root->left), height(root->right));
+		return 1 + max(height(node->left), height(node->right));
 	}
 
 	// Rotate Methods initially coded in Stepik
@@ -383,6 +416,23 @@ public:
 		return rotateLeft(node);
 	}
 
+	// Function to make sure created tree is AVL
+	bool checkAVL()
+	{
+		// Calls check on root node
+		return checkAVL(root);
+	}
+
+	bool checkAVL(Node* root) {
+		// From my Stepik, checks if tree is AVL
+		// https://stepik.org/submissions/1590562?unit=400654
+		// Helper function for me to check
+		if (root == nullptr)
+			return true;
+		if (abs(height(root->left) - height(root->right)) > 1)
+			return false;
+		return checkAVL(root->left) && checkAVL(root->right);
+	}
 
 private:
 	Node* root;
@@ -393,9 +443,10 @@ int main()
 {
 	//Initializes AVL
 	AVL ID;
-	
+
 	string line;
 	int count;
+	// Gets number of times program should iterate
 	getline(cin, line);
 	try {
 		count = stoi(line);
@@ -404,9 +455,8 @@ int main()
 		cout << "Fatal Error" << endl;
 		return 0;
 	}
-	
+
 	for (int i = 0; i < count; i++) {
-		//ID.printInorder();
 		getline(cin, line);
 		auto pos1 = line.find(" ");
 		string command = line.substr(0, pos1);
@@ -414,20 +464,20 @@ int main()
 		if (command.compare("insert") == 0) {
 			auto par1 = line.find("\"");
 			par1++;
-			auto par2 = line.find("\"",par1);
+			auto par2 = line.find("\"", par1);
 			string name = line.substr(par1, par2 - par1);
 
 			// Checks to make sure name is a valid input (no numbers)
 			bool valid = true;
 			for (unsigned int i = 0; i < name.size(); i++) {
-				if (!isalpha(name[i]) && !(name[i]==' ')) {
+				if (!isalpha(name[i]) && !(name[i] == ' ')) {
 					valid = false;
 					break;
 				}
 			}
 			par2 += 2;
 			string num = line.substr(par2);
-			
+
 			// Checks to make sure input is a 8 digit number
 			if (num.size() != 8)
 				valid = false;
@@ -445,7 +495,7 @@ int main()
 			catch (exception) {
 				valid = false;
 			}
-			
+
 			if (valid) {
 				ID.insert(name, idNum);
 			}
@@ -464,7 +514,7 @@ int main()
 					break;
 				}
 			}
-			if (num.size() != 8|| !(valid))
+			if (num.size() != 8 || !(valid))
 				cout << "unsuccessful" << endl;
 
 			else {
@@ -473,13 +523,13 @@ int main()
 			}
 		}
 		else if (command.compare("search") == 0) {
-			
+
 			if (line.find("\"") != string::npos) {
 				auto par1 = line.find("\"");
 				par1++;
 				auto par2 = line.find("\"", par1);
 				string name = line.substr(par1, par2 - par1);
-				
+
 				// Checks to make sure name contains no numbers
 				bool validName = true;
 				for (unsigned int i = 0; i < name.size(); i++) {
@@ -496,7 +546,7 @@ int main()
 				pos1++;
 				string num = line.substr(pos1);
 				int idNum;
-				
+
 				// Checks to see if number is all digits
 				bool valid = true;
 				for (unsigned int i = 0; i < num.length(); i++) {
@@ -505,7 +555,7 @@ int main()
 						break;
 					}
 				}
-				if (num.size() != 8 || !(valid) ) {
+				if (num.size() != 8 || !(valid)) {
 					cout << "unsuccessful" << endl;
 				}
 				else {
@@ -514,9 +564,9 @@ int main()
 						ID.search(idNum);
 					}
 					catch (exception) {
-						cout<<"unsuccessful"<<endl;
+						cout << "unsuccessful" << endl;
 					}
-					
+
 				}
 			}
 		}
